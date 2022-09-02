f4d19d43400c253313ee7a7a251ff32069f36572
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-02 09:22:28 +0600

    add dynamic data and handle error in grid

--- src/components/grid/VideoGrid.js
+++ src/components/grid/VideoGrid.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import VideoGridItem from './VideoGridItem';
+import { fetchVideos } from '../../features/videos/videosSlice';
+import Loading from '../ui/Loading';
 
 const VideoGrid = () => {
+  const dispatch = useDispatch();
+  const { videos, isLoading, isError, error } = useSelector((state) => state.videos);
+
+  useEffect(() => {
+    dispatch(fetchVideos());
+  }, [dispatch]);
+
+  // decide what to render
+  let content;
+
+  if (isLoading) content = <Loading />;
+  if (!isLoading && isError) content = <div className='col-span-12'>{error}</div>;
+
+  if (!isError && !isLoading && videos?.length === 0) {
+    content = <div className='col-span-12'>No videos found...!!!</div>;
+  }
+
+  if (!isError && !isLoading && videos?.length > 0) {
+    content = videos.map((video) => <VideoGridItem key={video.id} video={video} />);
+  }
+
   return (
     <section className='pt-12'>
       <section className='pt-12'>
-        <div className='grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]'>
-          <VideoGridItem />
-
-          {/* <div className="col-span-12">some error happened</div> */}
-        </div>
+        <div className='grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]'>{content}</div>
       </section>
     </section>
   );
